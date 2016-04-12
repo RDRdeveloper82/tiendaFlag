90c5260bd2ec35ae9cc79f821a09ce50324a791c
RDRdeveloper82 <RDRdeveloper82@gmail.com>
2016-04-12 14:43:14 +0200

    Subo Modelo de Backend admin.php

---
+++ backend/models/admin.php
@@ -0,0 +1,36 @@
+<?php
+namespace backend\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+
+class Admin extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
+{
+	public static function findIdentity($id){	
+		return static::findOne($id);	
+	}
+
+	public static function findIdentityByAccessToken($token, $type = null){	
+		throw new NotSupportedException();
+	}
+	
+	public function getId(){
+		return $this->idadmin;	
+	}
+	public function getAuthKey(){	
+		return $this->authkey;	
+	}
+	
+	public function validateAuthKey($authKey){	
+		return $this -> authkey === $authKey;
+	}
+	
+	public static function findByUsername($username){
+		return self::findOne(['username'=>$username]);
+	}
+	
+	public function validatePassword($password){
+		return $this->password === $password;
+	}
+}
